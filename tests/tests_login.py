3d72d395049607d055c98ce4e02456ce95c0c349
Karlen <arabadzhyank@gmail.com>
2025-11-01 10:59:01 +0300

    Объединены тестовые функции в классы по функциональности

--- tests/tests_login.py
+++ tests/tests_login.py
@@ -1,28 +1,33 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import LoginPageLocators
+import pytest
 
 
-def test_login(driver, login_data):
-
-    email, password = login_data
-    driver.find_element(*LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
-    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
-    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
-    assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGOUT_BUTTON))
-    
+@pytest.mark.usefixtures("driver")
+class TestLogin:
 
 
-def test_logout(driver, login_data):
+    def test_login(self, driver, login_data):
 
-    email, password = login_data
-    driver.find_element(*LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
-    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
-    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
-    driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click() 
-    assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGOUT_BUTTON))
-    
-    driver.find_element(*LoginPageLocators.LOGOUT_BUTTON).click() 
-    assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON))
+        email, password = login_data
+        driver.find_element(*LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
+        assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGOUT_BUTTON))
+        
+
+
+    def test_logout(self, driver, login_data):
+
+        email, password = login_data
+        driver.find_element(*LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
+        driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(email)
+        driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click() 
+        assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGOUT_BUTTON))
+        
+        driver.find_element(*LoginPageLocators.LOGOUT_BUTTON).click() 
+        assert WebDriverWait(driver, 5).until(EC.visibility_of_element_located(LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON))
     
--- tests/tests_registration_positive.py
+++ tests/tests_registration_positive.py
@@ -1,13 +1,21 @@
+import pytest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from locators import RegistrationPageLocators
 
 
-def test_registration(driver, random_email, random_password):
-    driver.find_element(*RegistrationPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
-    driver.find_element(*RegistrationPageLocators.NO_ACCOUNT_BUTTON).click()
-    driver.find_element(*RegistrationPageLocators.EMAIL_INPUT).send_keys(random_email)
-    driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT).send_keys(random_password)
-    driver.find_element(*RegistrationPageLocators.CONFIRM_PASSWORD_INPUT).send_keys(random_password)
-    driver.find_element(*RegistrationPageLocators.CREATE_ACCOUNT_BUTTON).click()
-    assert WebDriverWait(driver, 3).until(EC.visibility_of_element_located(RegistrationPageLocators.LOGOUT_BUTTON))
+
+@pytest.mark.usefixtures("driver")
+class TestRegistration:
+
+
+    def test_registration(self, driver, random_user):
+
+        email, password = random_user
+        driver.find_element(*RegistrationPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
+        driver.find_element(*RegistrationPageLocators.NO_ACCOUNT_BUTTON).click()
+        driver.find_element(*RegistrationPageLocators.EMAIL_INPUT).send_keys(email)
+        driver.find_element(*RegistrationPageLocators.PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*RegistrationPageLocators.CONFIRM_PASSWORD_INPUT).send_keys(password)
+        driver.find_element(*RegistrationPageLocators.CREATE_ACCOUNT_BUTTON).click()
+        assert WebDriverWait(driver, 3).until(EC.visibility_of_element_located(RegistrationPageLocators.LOGOUT_BUTTON))
