0228b0aaf5bfb0c16c39541606aebbc1bbc6ee28
Karlen <arabadzhyank@gmail.com>
2025-11-03 09:42:45 +0300

    Удалены фикстуры без логики действия и удалён генератор yield из login_user

--- conftest.py
+++ conftest.py
@@ -1,13 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from data.urls import Urls
 from data import test_data
 from helpers import generators
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from locators import LoginPageLocators
-
-
 
 
 @pytest.fixture
@@ -17,11 +15,6 @@ def driver():
     driver.get(Urls.BASE_URL)
     yield driver
     driver.quit()
-
-
-@pytest.fixture
-def login_data():
-    return test_data.LOGIN_EMAIL, test_data.LOGIN_PASSWORD
 
 
 @pytest.fixture
@@ -39,18 +32,16 @@ def ads_book_data():
 
 
 @pytest.fixture
-def login_user(driver, login_data):
-    
+def login_user(driver):
     driver.find_element(*LoginPageLocators.LOGIN_AND_REGISTRATION_BUTTON).click()
-    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(login_data[0])
-    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(login_data[1])
+    driver.find_element(*LoginPageLocators.EMAIL_INPUT).send_keys(test_data.LOGIN_EMAIL)
+    driver.find_element(*LoginPageLocators.PASSWORD_INPUT).send_keys(test_data.LOGIN_PASSWORD)
     driver.find_element(*LoginPageLocators.LOGIN_BUTTON).click()
 
-    
     WebDriverWait(driver, 5).until(
         EC.visibility_of_element_located(LoginPageLocators.LOGOUT_BUTTON)
     )
-    yield driver
+    return driver
 
 
 
